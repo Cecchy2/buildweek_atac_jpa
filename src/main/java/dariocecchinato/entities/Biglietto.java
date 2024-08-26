f074904028ec3cb828a024092a546a1d7d7fd820
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-08-26 15:09:28 +0200

    Entity Biglietto

--- src/main/java/dariocecchinato/entities/Biglietto.java
+++ src/main/java/dariocecchinato/entities/Biglietto.java
@@ -21,6 +21,88 @@ public class Biglietto {
     @ManyToOne
     @JoinColumn(name = "rivenditore_id (FK)")
     private Rivenditore rivenditore;
-    
+    @ManyToOne
+    @JoinColumn(name = "utente_id (FK)")
+    private Utente utente;
+    @OneToOne
+    @JoinColumn(name = "tessera_id")
+    private Tessera tessera;
 
+    public Biglietto() {
+    }
+
+    public Biglietto(LocalDate dataEmissione, double prezzo, Distributore distributore, Rivenditore rivenditore, Utente utente, Tessera tessera) {
+        this.dataEmissione = dataEmissione;
+        this.prezzo = prezzo;
+        this.distributore = distributore;
+        this.rivenditore = rivenditore;
+        this.utente = utente;
+        this.tessera = tessera;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public Distributore getDistributore() {
+        return distributore;
+    }
+
+    public void setDistributore(Distributore distributore) {
+        this.distributore = distributore;
+    }
+
+    public Rivenditore getRivenditore() {
+        return rivenditore;
+    }
+
+    public void setRivenditore(Rivenditore rivenditore) {
+        this.rivenditore = rivenditore;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public Tessera getTessera() {
+        return tessera;
+    }
+
+    public void setTessera(Tessera tessera) {
+        this.tessera = tessera;
+    }
+
+    @Override
+    public String toString() {
+        return "Biglietto{" +
+                "id=" + id +
+                ", dataEmissione=" + dataEmissione +
+                ", prezzo=" + prezzo +
+                ", distributore=" + distributore +
+                ", rivenditore=" + rivenditore +
+                ", utente=" + utente +
+                ", tessera=" + tessera +
+                '}';
+    }
 }
