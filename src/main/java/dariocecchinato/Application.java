f21efe0e29a3e5e04256ebabe09cf578abc0fbe9
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-08-26 19:44:19 +0200

    tratta dao add

--- src/main/java/dariocecchinato/Application.java
+++ src/main/java/dariocecchinato/Application.java
@@ -1,12 +1,37 @@
 package dariocecchinato;
 
+import com.github.javafaker.Faker;
+import dariocecchinato.dao.TrattaDao;
+import dariocecchinato.entities.Tratta;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Supplier;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("atac");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        TrattaDao trattaDao = new TrattaDao(em);
+        Faker faker = new Faker(Locale.ITALY);
+        Supplier<Tratta> trattaSupplier = () -> new Tratta(
+                faker.address().cityName(),
+                faker.address().cityName(),
+                faker.number().numberBetween(15, 120)
+        );
+        List<Tratta> tratte = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Tratta tratta = trattaSupplier.get();
+            tratte.add(tratta);
+        }
+        //tratte.forEach(trattaDao::save);
+        System.out.println("fin qui ci siamo...");
     }
+
+
 }
