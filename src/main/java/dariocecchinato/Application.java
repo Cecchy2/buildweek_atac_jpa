2717157c20cdef83441a2a550283f9c672126254
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-08-27 09:12:14 +0200

    Update Application.java

--- src/main/java/dariocecchinato/Application.java
+++ src/main/java/dariocecchinato/Application.java
@@ -1,12 +1,19 @@
 package dariocecchinato;
 
+import com.github.javafaker.Faker;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.util.Locale;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("atac");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        Faker faker = new Faker(Locale.ITALY);
+
+        
     }
 }
