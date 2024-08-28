b6895374f848b02b4b385a864f9fa09437c5198b
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-08-28 09:38:08 +0200

    metodi getById per utente ed amministratore

--- src/main/java/dariocecchinato/dao/AmministratoreDao.java
+++ src/main/java/dariocecchinato/dao/AmministratoreDao.java
@@ -1,11 +1,13 @@
 package dariocecchinato.dao;
 
 import dariocecchinato.entities.Amministratore;
+import dariocecchinato.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 
 import java.util.List;
+import java.util.UUID;
 
 public class AmministratoreDao {
     private final EntityManager em;
@@ -27,5 +29,11 @@ public class AmministratoreDao {
     public List<Amministratore> findAll() {
         TypedQuery<Amministratore> query = em.createQuery("SELECT a FROM Amministratore a", Amministratore.class);
         return query.getResultList();
+    }
+
+    public Amministratore getById(UUID amministratoreId) {
+        Amministratore found = em.find(Amministratore.class, amministratoreId);
+        if (found == null) throw new NotFoundException(amministratoreId);
+        return found;
     }
 }
